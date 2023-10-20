7519772efb30c4a3da3363e549d54789e5ca1d68
Dr Mike Cooper <mcooper@tantalus.com.au>
2023-10-20 16:39:35 +1030

    Drag and drop blueprints tutorial completed

---
+++ Source/DragAndDrop/DragAndDrop.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DragAndDrop.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragAndDrop, "DragAndDrop" );
+
+DEFINE_LOG_CATEGORY(LogDragAndDrop)
+ 
